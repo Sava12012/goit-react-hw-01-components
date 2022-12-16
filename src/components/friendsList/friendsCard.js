ce910f92d93c284152ceadb869eb5ea0353267b4
Dmytro Savych <sava.savych@gmail.com>
2022-12-16 18:42:02 +0200

    remowe width (friendsCard.css)

--- src/components/friendsList/friendsCard.js
+++ src/components/friendsList/friendsCard.js
@@ -6,7 +6,7 @@ export default function FriendsCard({ isOnline, avatar, name }) {
       <li className={css.item}>
         <span className={isOnline ? css.isonline : css.isoffline}>{isOnline}</span>
         <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-                <p className={css.name}>{name}</p>
+        <p className={css.name}>{name}</p>
             </li>
     ) 
 }
